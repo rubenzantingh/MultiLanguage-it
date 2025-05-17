e1968ba4f2ef6d14d15e060b276399c5bdd8b3b9
Ruben Zantingh <ruben@e-tales.nl>
2025-05-17 09:18:21 +0200

    Move translations to the core addon

--- MultiLanguage_IT.lua
+++ MultiLanguage_IT.lua
@@ -15,13 +15,6 @@ local function addLanguageOption()
         table.insert(MultiLanguageOptions.AVAILABLE_LANGUAGES, {value = 'it', text = 'Italian'})
         AddLanguageDropdownOption()
     end
-
-    if MultiLanguageTranslations['it'] == nil then
-        MultiLanguageTranslations['it'] = {
-            description = 'Descrizione',
-            objectives = 'Obiettivi della missione'
-        }
-    end
 end
 
 local function addonLoaded(self, event, addonLoadedName)
